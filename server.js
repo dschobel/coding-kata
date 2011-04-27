c0f90a55455bc6068faa2ed816f926528ccbcb70
dschobel <dschobel@gmail.com>
2011-04-28 06:20:17 +1000

    began work on async style / redis-backed version

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-var http = require('http');
-var cluster = require('cluster');
-var multinode = require('./lib/multi-node');
-var url = require('url');
+var http = require('http'),
+    url = require('url'),
+    redis = require("redis").createClient();
+
 
 PRODUCT_RPM = 40;
 GLOBAL_RPM = 100;
+FAIL_MESSAGE = 'missing client and/or product parameters\n';
 global_refill_rate = Math.round(GLOBAL_RPM /0.06);//convert to ms
 product_refill_rate = Math.round(PRODUCT_RPM/0.06);
 
 GlobalTokens = {};
 ProductTokens = {};
-RateToBucketsMap = {};
+RateToProductBucketsMap = {};
+RateToGlobalBucketsMap = {};
 IntervalIds ={};
 
-function TokenBucket(limit){
-    this.Limit = this.Tokens = limit;
-    this.getToken = function(){
-        if(this.Tokens < 1){
-            return false;
-        }
-        this.Tokens--;
-        return true;
+IntervalIds[global_refill_rate] = setInterval(refillEvent, global_refill_rate, global_refill_rate);
+IntervalIds[product_refill_rate] = setInterval(refillEvent, product_refill_rate, product_refill_rate);
+
+function TokenBucket(id, limit, client){
+    this.Client = client;
+    this.Limit = limit;
+    client.set(id,limit);
+    this.getToken = function(fn){
+        client.get(id,function(err,reply){
+        
+            if(reply < 1){ return fn(false); }
+            client.decr(id, fn(true));
+        });
    };
 }
 function refillEvent(rate){
@@ -45,36 +52,53 @@ function registerBucket(rate,bucket){
         if(!IntervalIds[rate]){
             console.log('bucket map does not contain a timer for rate: ' + rate);
             //IntervalIds[rate] = 'foo';
-            IntervalIds[rate] = setInterval(refillEvent,rate,rate);
         }
         else{console.log('reusing existing timer for rate: ' + rate);}
 
 }
 
-function isRateLimited(client, product){
-    var productKey = client + product;
+function isRateLimited(clientid, productid, fn){
+    var productidKey = clientid + productid;
 
-    //if the bucket for this client doesn't exist, create it
-    if(!GlobalTokens[client]){
-        console.log('bucket for client \'' + client + '\' does not exist, creating it');
-        GlobalTokens[client] = new TokenBucket(GLOBAL_RPM);
+    //handle global tokens
+    redis.hexists(GlobalTokens,clientid,function(err,response){
+        if(!response){
+          redis.hset(GlobalTokens,clientid,GLOBAL_RPM,fn(err,reply)
+          {   
+                console.log("first time seeing " + clientid + ", setting tokens to: " + GLOBAL_RPM);
+          }
+
+          redis.hset("GlobalRateToBucketsMap","
+          registerTimer("global",global_refill_rate, clientid)
+          fn(null,false);
+        }
+
+
+    }
+
+    //handle product tokens
+
+    //if the bucket for this clientid doesn't exist, create it
+    if(!GlobalTokens[clientid]){
+        console.log('bucket for clientid \'' + clientid + '\' does not exist, creating it');
+        GlobalTokens[clientid] = new TokenBucket(GLOBAL_RPM);
         //register the new bucket in the rate : buckets map
-        registerBucket(global_refill_rate, GlobalTokens[client]);
+        registerBucket(global_refill_rate, GlobalTokens[clientid]);
     }
-    if(!ProductTokens[productKey]){
-        console.log('bucket for productkey \'' + productKey + '\' does not exist, creating it');
-        ProductTokens[productKey] = new TokenBucket(PRODUCT_RPM);
-        registerBucket(product_refill_rate, ProductTokens[productKey]);
+    if(!productidTokens[productidKey]){
+        console.log('bucket for productidkey \'' + productidKey + '\' does not exist, creating it');
+        productidTokens[productidKey] = new TokenBucket(productid_RPM);
+        registerBucket(productid_refill_rate, productidTokens[productidKey]);
     }
 
-    if(!GlobalTokens[client].getToken()){
-        console.log('hit global limit for ' + client);
+    if(!GlobalTokens[clientid].getToken()){
+        console.log('hit global limit for ' + clientid);
         return {limited:true,limittype:'global'};
     }
 
-    if(!ProductTokens[productKey].getToken()){
-        console.log('hit product limit for ' + productKey);
-        return {limited:true,limittype:'product'};
+    if(!productidTokens[productidKey].getToken()){
+        console.log('hit productid limit for ' + productidKey);
+        return {limited:true,limittype:'productid'};
     }
 
     return {limited:false};
@@ -86,7 +110,6 @@ console.log('global refill rate will yield a new request every: ' + global_refil
 console.log('product refill rate will yield a new request every: ' + product_refill_rate + ' ms');
 
 
-FAIL_MESSAGE = 'missing client and/or product parameters\n';
 var s = http.createServer(function(req,res){
     var query = url.parse(req.url, true).query;
     if(!query.client || !query.product){
@@ -99,19 +122,9 @@ var s = http.createServer(function(req,res){
         return;
     }
     res.writeHead(200, { 'Content-Type':'application/json' });
-    
-    var result = isRateLimited(query.client,query.product);
-    if(result.limited)
-    {
-        res.end(JSON.stringify({limited:true,limittype:result.limittype,client:query.client, product:query.product}));
-    }
-    else{
-        res.end(JSON.stringify({limited:false,limittype:result.limittype,client:query.client,product:query.product}));
-    }
+    isRateLimited(query.client,query.product,console.log,function(){
+            res.end(JSON.stringify({limited:result.limited,limittype:result.limittype,client:query.client,product:query.product}));
+    });
 });
 
-/*var nodes = multinode.listen({
-    port: 8000,
-    nodes: 2}, s);
-cluster(s).listen(8000);*/
 s.listen(8000);
